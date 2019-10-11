6e48f9e73640be74dde00bc10a5b7dcd53f2b969
Steven Avelino <steven.avelino24@outlook.com>
2019-10-11 15:26:20 +0200

    Add links to restricted API examples

--- examples/index.php
+++ examples/index.php
@@ -19,11 +19,25 @@ include_once "templates/base.php";
 
 <?= pageHeader("TrustedShops API SDK Examples"); ?>
 
+<h2>Common API</h2>
+
 <ul>
   <li><a href="common/shops/get_shops_by_id.php">Get Shops from a TrustedShopsID Example</a></li>
   <li><a href="common/shops/get_shops_by_url.php">Get Shops from an URL Example</a></li>
   <li><a href="common/reviews/get_reviews.php">Get Reviews for a shop from a TrustedShops ID</a></li>
   <li><a href="common/quality_indicators/get_quality_indicators.php">Get Quality Indicators for a shop from a TrustedShops ID</a></li>
+</ul>
+
+<h2>Restricted API</h2>
+
+<ul>
+  <li><a href="restricted/shops/get_shops.php">Get Shops from a TrustedShopsID Example</a></li>
+  <li><a href="restricted/reviews/get_reviews.php">Get Reviews from a TrustedShopsID Example</a></li>
+  <li><a href="restricted/quality_indicators/get_quality.php">Get Quality Indicators from a TrustedShopsID Example</a></li>
+  <li><a href="restricted/quality_indicators/get_quality_complaints.php">Get Quality Indicators Complaints from a TrustedShopsID Example</a></li>
+  <li><a href="restricted/quality_indicators/get_quality_reviews.php">Get Quality Indicators Reviews from a TrustedShopsID Example</a></li>
+  <li><a href="restricted/benchmarks/get_benchmarks.php">Get Benchmarks from a TrustedShopsID Example</a></li>
+  <li><a href="restricted/retailer_shops/get_retailers.php">Get Retailers from a TrustedShopsID Example</a></li>
 </ul>
 
 <?= pageFooter(); ?>
