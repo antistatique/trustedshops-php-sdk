4200cfe60eb465a835e0b404f4664cf2f452bb4f
Steven Avelino <steven.avelino24@outlook.com>
2019-10-11 11:09:32 +0200

    Create base template

---
+++ examples/templates/base.php
@@ -0,0 +1,60 @@
+<?php
+
+include_once \dirname(__DIR__) . '/../vendor/autoload.php';
+
+$env =  \dirname(__DIR__) . '/.env';
+
+if (file_exists($env)) {
+  $dotenv = new Symfony\Component\Dotenv\Dotenv($env);
+  $dotenv->load($env);
+}
+
+function getEnvVariables() {
+  return [
+    'TRUSTEDSHOPS_ID' => getenv('TRUSTEDSHOPS_TSID'),
+    'TRUSTEDSHOPS_LOGIN' => getenv('TRUSTEDSHOPS_LOGIN'),
+    'TRUSTEDSHOPS_PASS' => getenv('TRUSTEDSHOPS_PASS')
+  ];
+}
+
+/* Ad hoc functions to make the examples marginally prettier.*/
+function isWebRequest()
+{
+  return isset($_SERVER['HTTP_USER_AGENT']);
+}
+
+function pageHeader($title)
+{
+  $ret = "<!doctype html>
+  <html>
+  <head>
+    <title>" . $title . "</title>
+    <link href='styles/style.css' rel='stylesheet' type='text/css' />
+  </head>
+  <body>\n";
+  if ($_SERVER['PHP_SELF'] != "/index.php") {
+    $ret .= "<p><a href='/index.php'>Back</a></p>";
+  }
+  $ret .= "<header><h1>" . $title . "</h1></header>";
+
+ // Start the session (for storing access tokens and things)
+  if (!headers_sent()) {
+    session_start();
+  }
+
+  return $ret;
+}
+
+function pageFooter($file = null)
+{
+  $ret = "";
+  if ($file) {
+    $ret .= "<h3>Code:</h3>";
+    $ret .= "<pre class='code'>";
+    $ret .= htmlspecialchars(file_get_contents($file));
+    $ret .= "</pre>";
+  }
+  $ret .= "</html>";
+
+  return $ret;
+}
