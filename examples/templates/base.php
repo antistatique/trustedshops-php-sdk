f8c8a34e218dc128c3ae39361401e6a73829aef2
Kevin Wenger <wenger.kev@gmail.com>
2019-11-01 15:23:43 +0100

    ensure the environement of examples is ready - improve dx experience by preventing intempestive crash when you don't follow the dev-process

--- examples/templates/base.php
+++ examples/templates/base.php
@@ -2,11 +2,17 @@
 
 include_once \dirname(__DIR__) . '/../vendor/autoload.php';
 
+if (!\class_exists('Symfony\Component\Dotenv\Dotenv')) {
+  throw new \RuntimeException('You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+}
+
 $env =  \dirname(__DIR__) . '/.env';
 
 if (file_exists($env)) {
   $dotenv = new Symfony\Component\Dotenv\Dotenv($env);
   $dotenv->load($env);
+} else {
+  throw new \RuntimeException('You need to define a .env file.');
 }
 
 function getEnvVariables() {
